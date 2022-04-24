5757e397d0642b794c384674aa33389871c06111
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-04-24 15:53:03 +0900

    Update and rename mergeSort.py to mergeSort+merge.py

--- mergeSort+merge.py
+++ mergeSort+merge.py
@@ -1,4 +1,4 @@
-def merge_sort(array):
+def merge_sort(array):              #병합 정렬
     if (len(array)<=1):
         return array
     else:
@@ -6,14 +6,14 @@ def merge_sort(array):
         left = merge_sort(array[0:mid])
         right = merge_sort(array[mid:len(array)])
     return merge(left, right)
-def merge(S1, S2): # S라는 빈 리스트 에 append 시키는 방법을 이용한 merge함수다.
+def merge(S1, S2):                  # S라는 빈 리스트 에 append 시키는 방법을 이용한 merge함수다.
     S = []
     i = j = 0
     while(i<len(S1) and j<len(S2)) :
-        if S1[i]<S2[j]: # 각 리스트의 크기를 비교 하여 작은것을 append 한다.
+        if S1[i]<S2[j]:             # 각 리스트의 크기를 비교 하여 작은것을 append 한다.
             S.append(S1[i])
             i+=1
-        else:           # 각 리스트의 크기를 비교 하여 작은것을 append 한다.
+        else:                       # 각 리스트의 크기를 비교 하여 작은것을 append 한다.
             S.append(S2[j])
             j+=1
     if i < len(S1):       
@@ -23,8 +23,9 @@ def merge(S1, S2): # S라는 빈 리스트 에 append 시키는 방법을 이용
     return S
     
     
+    #################################################################################
     
-def merge(S1,S2,S):
+def merge(S1,S2,S):                 
     i = j = 0
     while i+j<len(S):
         if j ==len(S2) or (i<len(S1)and S1[i]<S2[j]):
